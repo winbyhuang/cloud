b69f087b7e26cb28516a4889654f9cc3004fd145
huangwenbo <huangwenbo@cmhi.chinamobile.com>
2018-12-29 18:04:10 +0800

    init

---
+++ eureka/src/main/java/com/winby/cloud/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.winby.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Winby
+ * @date 2018/12/29  17:20
+ * @update
+ * @since v1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
