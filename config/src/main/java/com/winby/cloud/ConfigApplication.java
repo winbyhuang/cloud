b38cd2113721ec51dc63d9bd19bb901be49c2673
winby <361831543@qq.com>
2018-12-31 04:32:51 +0800

    config

---
+++ config/src/main/java/com/winby/cloud/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.winby.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Winby
+ * @date 2018/12/29  17:20
+ * @update
+ * @since v1.0.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
