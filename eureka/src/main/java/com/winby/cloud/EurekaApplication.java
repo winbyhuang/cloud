22657861a399e956895aaad4c263d8f5b5cedb98
winby <361831543@qq.com>
2018-12-31 00:17:00 +0800

    eureka

--- eureka/src/main/java/com/winby/cloud/EurekaApplication.java
+++ eureka/src/main/java/com/winby/cloud/EurekaApplication.java
@@ -13,7 +13,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(EurekaApplication.class, args);
     }
