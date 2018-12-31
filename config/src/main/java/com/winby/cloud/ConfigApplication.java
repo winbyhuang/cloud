c1fb16b4268849291e790852d15c99eb126f84b9
winby <361831543@qq.com>
2018-12-31 15:07:00 +0800

    config

--- config/src/main/java/com/winby/cloud/ConfigApplication.java
+++ config/src/main/java/com/winby/cloud/ConfigApplication.java
@@ -3,6 +3,7 @@ package com.winby.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Winby
@@ -11,7 +12,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @since v1.0.0
  */
 @SpringBootApplication
+@EnableEurekaClient
 @EnableConfigServer
+//@EnableDiscoveryClient
 public class ConfigApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ConfigApplication.class, args);
