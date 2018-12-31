e938b68519013505c68c8ac9c7138fd685f0274e
winby <361831543@qq.com>
2018-12-31 15:50:31 +0800

    config

--- eureka/src/main/java/com/winby/cloud/EurekaApplication.java
+++ eureka/src/main/java/com/winby/cloud/EurekaApplication.java
@@ -1,8 +1,14 @@
 package com.winby.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Winby
@@ -12,7 +18,19 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
+@RefreshScope
 public class EurekaApplication {
+    @Value("cloud.eureka.peer.port")
+    String value;
+
+    @Autowired
+    Environment environment;
+
+    @RequestMapping("he")
+    public String he(){
+        return environment.getProperty("cloud.eureka.peer.name");
+    }
     public static void main(String[] args) throws Exception {
         SpringApplication.run(EurekaApplication.class, args);
     }
