86b31ed5f785954390432c5c4d0ef08845923ff0
winby <361831543@qq.com>
2018-12-31 19:03:49 +0800

    config

--- common/src/main/java/com/winby/cloud/CommonApplication.java
+++ common/src/main/java/com/winby/cloud/CommonApplication.java
@@ -1,7 +1,11 @@
 package com.winby.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Winby
@@ -11,6 +15,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class CommonApplication {
+    @Value("cloud.eureka.peer.port")
+    String value;
+    @Value("myww")
+    String my;
+
+    @Autowired
+    Environment environment;
+    @RequestMapping("he")
+    public String he(){
+        return my;
+//        return environment.getProperty("server.port");
+    }
     public static void main(String[] args) throws Exception {
         SpringApplication.run(CommonApplication.class, args);
     }
